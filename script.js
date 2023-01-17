72eb111c61ed9faadb3ee19e27ecfad901f1f3b6
Vivek Shirsath <vivekshirsath1310@gmail.com>
2023-01-17 12:50:33 +0530

    Switching Active Player

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+const currentScore0El = document.querySelector("#current--0");
+const currentScore1El = document.querySelector("#current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 //Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling Dice Functionality
 btnRoll.addEventListener("click",function(){
@@ -25,10 +30,15 @@ btnRoll.addEventListener("click",function(){
 
     //check if roll 1
     if(dice!==1){
-
+        currentScore += dice;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     }
     else{
-
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 1 ? 0 : 1;
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
 
 })
